d76e27413ae18ed7de4c85e5e9d7418c4afc34fd
kamranshah125 <kamran.shah@venturequeue.com>
2025-12-10 19:17:48 +0500

    Update TrustProxies middleware configuration

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,28 +2,25 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
     /**
-     * The trusted proxies for this application.
+     * Trust all proxies (important for DigitalOcean App Platform)
      *
-     * @var array<int, string>|string|null
+     * @var string|array|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
-     * The headers that should be used to detect proxies.
+     * Headers used to detect proxy protocol / host info.
      *
      * @var int
      */
-    // protected $headers =
-    //     Request::HEADER_X_FORWARDED_FOR |
-    //     Request::HEADER_X_FORWARDED_HOST |
-    //     Request::HEADER_X_FORWARDED_PORT |
-    //     Request::HEADER_X_FORWARDED_PROTO |
-    //     Request::HEADER_X_FORWARDED_AWS_ELB;
-     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO;
 }
